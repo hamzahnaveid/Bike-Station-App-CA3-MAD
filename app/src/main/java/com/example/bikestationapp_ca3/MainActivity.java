47cb3198424362e5723a8335ef9bc57791b81b92
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2025-12-10 23:24:55 +0000

    User registration

--- app/src/main/java/com/example/bikestationapp_ca3/MainActivity.java
+++ app/src/main/java/com/example/bikestationapp_ca3/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.bikestationapp_ca3;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.bikestationapp_ca3.classes.User;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        sp = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
     }
+
+    public void toRegisterScreen(View view) {
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void signIn(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(
+                    this, "Please fill in the required fields",
+                    Toast.LENGTH_SHORT
+            ).show();
+            return;
+        }
+
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putString("EMAIL", email);
+        editor.commit();
+
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/bikestationapp_ca3/RegisterActivity.java
@@ -0,0 +1,122 @@
+package com.example.bikestationapp_ca3;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.bikestationapp_ca3.classes.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    EditText etEmail, etPassword, etName;
+    SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        auth = FirebaseAuth.getInstance();
+        sp = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+
+        etEmail = findViewById(R.id.et_registeremail);
+        etPassword = findViewById(R.id.et_registerpassword);
+        etName = findViewById(R.id.et_name);
+    }
+
+    public void register(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String name = etName.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty() || name.isEmpty()) {
+            Toast.makeText(this,
+                    "Please fill in all of the required fields",
+                    Toast.LENGTH_SHORT
+            ).show();
+            etEmail.setText("");
+            etPassword.setText("");
+            etName.setText("");
+            return;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(this,
+                    "Password must be at least 6 characters long",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+
+        User user = new User(email, password, name);
+        String userKey = addUserToDb(user);
+
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putString("USER", userKey);
+        editor.commit();
+
+        Intent intent = new Intent(RegisterActivity.this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    public String addUserToDb(User user) {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference("users");
+
+        String key = ref.push().getKey();
+
+        ref.child(key).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                addUserToAuth(user.getEmail(), user.getPassword());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this,
+                        e.getMessage(),
+                        Toast.LENGTH_SHORT
+                ).show();
+                e.printStackTrace();
+            }
+        });
+        return key;
+    }
+
+    public void addUserToAuth(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Toast.makeText(RegisterActivity.this,
+                        "User successfully registered",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+    }
+}
